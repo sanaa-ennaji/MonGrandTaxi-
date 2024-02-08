1ae223684b69ad8f090626f1b7b6fd3fd93c86f4
sanaa <sanaaennaji93@gmail.com>
2024-02-08 11:15:39 +0100

    laravel

---
+++ app/Http/Controllers/api/PostController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+    // $posts = Post::all();
+    //   return response()->json([
+    //      'statu' => true,
+    //       'postS' => $posts
+    //   ],200);
+
+    }
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(post $post)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/passangerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class passangerController extends Controller
+{
+   public function store(){
+    $passanger = new passanger;
+   }
+}
